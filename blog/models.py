6d3485bfa8bc89a553f37581bc866726d83ca150
shokha-dude <shokha_dude@mail.ru>
2024-11-20 08:43:49 +0400

    added post_list and post_detail views

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
+
 
 class Post(models.Model):
     class Status(models.TextChoices):
@@ -22,11 +27,13 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+    objects = models.Manager()  # менеджер, применяемый по умолчанию
+    published = PublishedManager()
 
-    class Meta:
-        ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish']),
+class Meta:
+    ordering = ['-publish']
+    indexes = [
+         models.Index(fields=['-publish']),
         ]
 
     def __str__(self):
