b1ba4e83c46b2087b57fac525a673531d5180e8b
shokha-dude <shokha_dude@mail.ru>
2024-11-27 14:18:41 +0400

    added migration

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,35 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
-def post_list(request):
-    posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts':posts})
 
-def post_detail(request, id):
-    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
+class PostListView(ListView):
+    """
+    Альтернативное представление списка постов
+    """
+    queryset = Post.published.all()
+    context_object_name = 'posts'
+    paginate_by = 3
+    template_name = 'blog/post/list.html'
+
+# def post_list(request):
+#     post_list = Post.published.all()
+#     paginator = Paginator(post_list, 3)
+#     page_number = request.GET.get('page', 1)
+#     try:
+#         posts = paginator.page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.page(1)
+#     except EmptyPage:
+#         posts = paginator.page(paginator.num_pages)
+#     return render(request, 'blog/post/list.html', {'posts':posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post,
+                             status=Post.Status.PUBLISHED,
+                             slug=post,
+                             publish__year=year,
+                             publish__month=month,
+                             publish__day=day)
     return render(request, 'blog/post/detail.html', {'post':post})
